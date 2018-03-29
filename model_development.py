9fec65c848f5a250dea3c66691b4ce34c070c9bc
The-Ben <ben@theaarons.com>
2018-03-29 07:56:16 -0700

    trying more epochs

--- model_development.py
+++ model_development.py
@@ -25,4 +25,4 @@ filepath="models/weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 
-model.fit(X, y, epochs=1, batch_size=128, callbacks=callbacks_list)
+model.fit(X, y, epochs=10, batch_size=128, callbacks=callbacks_list)
